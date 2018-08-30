760d3be81947d70cda80c8ece1086219b07e43fc
Mike <mmarceca@aardvarkbrigade.com>
2018-08-30 14:14:57 -0400

    added cards and base skeleton

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// app.use(express.static(path.join(__dirname + '/styles')));
-
-app.get('/assets/styles/main.css', (req,res) => {
-  res.sendFile(path.join(__dirname + '/assets/styles/main.css'))
-})
+app.use(express.static('static'));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname + '/html/index.html'));
+  res.sendFile(path.join(__dirname + '/templates/index.html'));
 });
 
 app.get('/black-jack', (req,res) => {
-  res.sendFile(path.join(__dirname + '/html/black-jack.html'));
+  res.sendFile(path.join(__dirname + '/templates/black-jack.html'));
 });
 
 app.listen(3000);
